be5740ba4da6aa248fae5815258a17b55314eff5
andymtv <andy.mtv12@gmail.com>
2021-03-08 21:07:39 +0100

    Updates

---
+++ precache-manifest.f3b483a598c0edbac4d7b06e66270317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a936368e70e28dd097b78b78014bf614",
+    "url": "/react-pomodoro-timer/index.html"
+  },
+  {
+    "revision": "1d9b1d2616b0ef80c020",
+    "url": "/react-pomodoro-timer/static/css/main.5d258144.chunk.css"
+  },
+  {
+    "revision": "d06aa85e6cde01125914",
+    "url": "/react-pomodoro-timer/static/js/2.218f4da8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-pomodoro-timer/static/js/2.218f4da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9b1d2616b0ef80c020",
+    "url": "/react-pomodoro-timer/static/js/main.a4381549.chunk.js"
+  },
+  {
+    "revision": "0995e7579e2a95f98dd8",
+    "url": "/react-pomodoro-timer/static/js/runtime-main.37b59aad.js"
+  }
+]);
